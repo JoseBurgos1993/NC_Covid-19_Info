c84b9608463c08b980541ded33bf6c95d483a2c4
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-06-25 21:33:14 -0400

    trying to push

--- assets/script.js
+++ assets/script.js
@@ -4,8 +4,9 @@
 $("document").ready(function(){
             
     // GLOBAL VARIABLES \\
-    const queryURL = "https://services.arcgis.com/iFBq2AW9XO0jYYF7/arcgis/rest/services/NCCovid19/FeatureServer/0/query?where=1%3D1&outFields=CONAME,County,Total,Deaths,StateCases,USCases,USDeaths,CountyCases,Hosp,Pop2018,Rate10k,COCAPS&outSR=4326&f=json";
+    const queryURL = "https://services.arcgis.com/iFBq2AW9XO0jYYF7/arcgis/rest/services/NCCovid19/FeatureServer/0/query?where=1%3D1&outFields=CONAME,County,Total,Deaths,StateCases,USCases,USDeaths,CountyCases,Hosp,Pop2018,Rate10k&outSR=4326&f=json";
 
+    var data;
 
     // FUNCTIONS \\
     function runAjax(){
@@ -14,31 +15,42 @@ $("document").ready(function(){
             
             const name = "Transylvania".toLowerCase(); // <---- You can type the county name here for testing
 
-            getCountyData(name);
+            countyData = getCountyData(name);
+            if(countyData === null){
+                alert("This county does not exist in North Carolina!");
+            } else{
+                /*
+                example of how we would query
+
+                $("#countyName")  .val(countyData.attributes.County);
+                $("#countyDeaths").val(countyData.attributes.Deaths);
+                $("#countyCases") .val(countyData.attributes.CountyCases);
+                */
+            }
         });
     }
 
     function getCountyData(name){
-        console.log("HERE");
         // There are 100 counties
         for(let i=0; i<100; i++){
             const tempCoName = data.features[i].attributes.CONAME.toLowerCase();
             const tempCounty = data.features[i].attributes.County.toLowerCase();
             if(name === tempCoName || name === tempCounty){
-                console.log(data.features[i]);
-                break; // Ends FOR loop
+                console.log(data.features[i].attributes);
+                return data.features[i].attributes;
             }
         }
+        return null;
     }
 
 
     // EVENT LISTENERS \\
-
+    
 
 
     // MAIN \\
     function main(){
-        var data = runAjax();
+        runAjax();
     }
     main();
 });
