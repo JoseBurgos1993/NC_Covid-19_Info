cea66ee510e7de6fbf074e6e95fb44d106321b3d
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-06-27 11:26:30 -0400

    Updated api calls

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,59 @@
 
 // DATA needed - number of cases, number of deaths, complete tests, number hospitalized
-
+// 
 $("document").ready(function(){
             
     // GLOBAL VARIABLES \\
-    const queryURL = "https://services.arcgis.com/iFBq2AW9XO0jYYF7/arcgis/rest/services/NCCovid19/FeatureServer/0/query?where=1%3D1&outFields=CONAME,County,Total,Deaths,StateCases,USCases,USDeaths,CountyCases,Hosp,Pop2018,Rate10k&outSR=4326&f=json";
 
     var data;
 
     // FUNCTIONS \\
-    function runAjax(){
+    function runAjax(name, number){
+        // The parameter 'number' is for determining whether this is the main page loading (1), or the user
+        // is on the county tab and pressed the submit button (2). I do this because javascript is really
+        // annoying.
+
+        const queryURL = "https://services.arcgis.com/iFBq2AW9XO0jYYF7/arcgis/rest/services/NCCovid19/FeatureServer/0/query?where=1%3D1&outFields=CONAME,County,Total,Deaths,StateCases,USCases,USDeaths,CountyCases,Hosp,Pop2018,Rate10k&outSR=4326&f=json";
+
         $.ajax({url: queryURL, method: "GET"}).then(function(response){
             data = JSON.parse(response);
             
-            const name = "Transylvania".toLowerCase(); // <---- You can type the county name here for testing
+            countyData = getCountyData(name.toLowerCase());
 
-            countyData = getCountyData(name);
             if(countyData === null){
                 alert("This county does not exist in North Carolina!");
             } else{
-                    
-                /*
-                example of how we would query
+                if(number == 1){ // State
+                    /*
+                    $("#TotalDeaths").val(countyData.attributes.TotalDeaths);
+                    $("#TotalCases").val(countyData.attributes.TotalCases);
+                    $("#Tested").val(countyData.attributes.Tested);
+                    $("#Hospitalized").val(countyData.attributes.Hospitalized);
+                    */
 
-                $("#countyName")  .val(countyData.attributes.County);
-                $("#countyDeaths").val(countyData.attributes.Deaths);
-                $("#countyCases") .val(countyData.attributes.CountyCases);
-                */
+                } else if(number == 2){ // County
+                    /*
+                    $("#countyName").val(countyData.attributes.County);
+                    $("#countyDeaths").val(countyData.attributes.Deaths);
+                    $("#countyCases").val(countyData.attributes.CountyCases);
+                    */
+                }
             }
+        });
+    }
+
+    function getLocation(){
+        //const queryURL = "https://data.healthcare.gov/resource/geocodes-usa-with-counties.json";
+        const queryURL = "https://data.healthcare.gov/resource/geocodes-usa-with-counties.json?state=NC";
+        $.ajax({url: queryURL, method: "GET"}).then(function(response){
+            console.log(response);
         });
     }
 
     function getCountyData(name){
         // There are 100 counties
         for(let i=0; i<100; i++){
+            // Compares county names and returns the correct one
             const tempCoName = data.features[i].attributes.CONAME.toLowerCase();
             const tempCounty = data.features[i].attributes.County.toLowerCase();
             if(name === tempCoName || name === tempCounty){
@@ -46,12 +66,42 @@ $("document").ready(function(){
 
 
     // EVENT LISTENERS \\
-    
+    /*
+    $(".submit").on("click", function(){
+        const name = $("#textField").val();
+        runAjax(name,2);
+    });
+
+    */
 
 
     // MAIN \\
     function main(){
-        runAjax();
+        getLocation();
+
+        //runAjax("Transylvania", 1); // <---- You can type the county name here for testing
     }
     main();
+    ////////////////////////
+
+    //
+    function findCounty(position){
+        const queryURL = "https://geo.fcc.gov/api/census/block/find?latitude=" + position.coords.latitude + "&longitude=" + position.coords.longitude + "&format=json";
+        $.ajax({url: queryURL, method: "GET"}).then(function(response){
+            console.log("FCC API");
+            console.log(response);
+        });
+    }
+
+    function getLocation() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(findCounty);
+        } else {
+            alert("Geolocation is not supported by this browser.");
+        }
+    }
+
+    //function showPosition(position) {
+    //    alert("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+    //}
 });
