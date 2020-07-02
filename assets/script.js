1f03ce741e7a259dc3661a5a23a4e50cf6e52910
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-07-02 14:15:22 -0400

    Put everything together. Jotform works.

--- assets/script.js
+++ assets/script.js
@@ -1,115 +1,85 @@
 
-// DATA needed - number of cases, number of deaths, complete tests, number hospitalized
-// 
-$("document").ready(function(){
-            
-    // GLOBAL VARIABLES \\
+// GLOBAL VARIABLES \\
 
-    var data;
+var data;
 
-    // FUNCTIONS \\
-    function runAjax(name, number){
-        // The parameter 'number' is for determining whether this is the main page loading (1), or the user
-        // is on the county tab and pressed the submit button (2). I do this because javascript is really
-        // annoying.
+// FUNCTIONS \\
+function runAjax(name, number){
+    // The parameter 'number' is for determining whether this is the main page loading (1), or the user
+    // is on the county tab and pressed the submit button (2). I do this because javascript is really
+    // annoying.
 
-        const queryURL = "https://services.arcgis.com/iFBq2AW9XO0jYYF7/arcgis/rest/services/NCCovid19/FeatureServer/0/query?where=1%3D1&outFields=CONAME,County,Total,Deaths,StateCases,USCases,USDeaths,CountyCases,Hosp,Pop2018,Rate10k&outSR=4326&f=json";
+    const queryURL = "https://services.arcgis.com/iFBq2AW9XO0jYYF7/arcgis/rest/services/NCCovid19/FeatureServer/0/query?where=1%3D1&outFields=CONAME,County,Total,Deaths,StateCases,USCases,USDeaths,CountyCases,Hosp,Pop2018,Rate10k&outSR=4326&f=json";
 
-        //const queryURL = "https://services.arcgis.com/iFBq2AW9XO0jYYF7/arcgis/rest/services/NCCovid19/FeatureServer/0/query?where=1%3D1&outSR=4326&f=json";
-
-        $.ajax({url: queryURL, method: "GET"}).then(function(response){
-            data = JSON.parse(response);
-            countyData = getCountyData(name.toLowerCase());
-            console.log(data);
-            if(countyData === null){
-                alert("This county does not exist in North Carolina!");
-            } else{
-                if(number == 1){ // State
-                    $("#totaldeaths").text(countyData.Total);
-                    $("#totalcases").text(countyData.StateCases);
-                    $("#rateper10k").text(countyData.Rate10k.toFixed(1));
-                    $("#hospitalized").text(countyData.Hosp);
-
-                } else if(number == 2){ // County
-                    $("#totaldeaths").text(countyData.Deaths);
-                    $("#totalcases").text(countyData.CountyCases);
-                }
-            }
-        });
-    }
-
-    function getLocation(){
-        const queryURL = "https://data.healthcare.gov/resource/geocodes-usa-with-counties.json?state=NC";
-        $.ajax({url: queryURL, method: "GET"}).then(function(response){
-            console.log(response);
-        });
-    }
-
-    function getCountyData(name){
-        // There are 100 counties
-        for(let i=0; i<100; i++){
-            // Compares county names and returns the correct one
-            const tempCoName = data.features[i].attributes.CONAME.toLowerCase();
-            const tempCounty = data.features[i].attributes.County.toLowerCase();
-            if(name === tempCoName || name === tempCounty){
-                return data.features[i].attributes;
-            }
-        }
-        return null;
-    }
-
-
-    // EVENT LISTENERS \\
-    
-    $(".search").on("click", function(){
-        const name = $("#searchbar").val();
-        runAjax(name,2);
-    });
-
-    $(".findme").on("click", getLocation);
-
-    $(".translator").on("click", function(){
-        if($(this).text() === "ESPAÑOL"){
-            /*
-            Do this for each item needing translation on the page.
-            CHANGE TO ENGLISH
-            $(thing).text("New words");
-            */
+    $.ajax({url: queryURL, method: "GET"}).then(function(response){
+        data = JSON.parse(response);
+        countyData = getCountyData(name.toLowerCase());
+        console.log(data);
+        if(countyData === null){
+            alert("This county does not exist in North Carolina!");
         } else{
-            /*
-            Do this for each item needing translation on the page.
-            CHANGE TO SPANISH
-            $(thing).text("New words");
-            */
+            if(number == 1){ // State
+                $("#totaldeaths").text(" " + countyData.Total + " ");
+                $("#totalcases").text(" " + countyData.StateCases);
+                $("#rateper10k").text(" " + countyData.Rate10k.toFixed(1) + " ");
+                $("#hospitalized").text(" " + countyData.Hosp + " ");
+
+            } else if(number == 2){ // County
+                $("#totaldeaths").text(" " + countyData.Deaths + " ");
+                $("#totalcases").text(" " + countyData.CountyCases + " ");
+            }
         }
     });
+}
 
-    // MAIN \\
-    function main(){
-        runAjax("Transylvania", 1); // <---- You can type the county name here for testing
-    }
-    main();
-    ////////////////////////
+function getLocation(){
+    const queryURL = "https://data.healthcare.gov/resource/geocodes-usa-with-counties.json?state=NC";
+    $.ajax({url: queryURL, method: "GET"}).then(function(response){
+        console.log(response);
+    });
+}
 
-    //
-    function findCounty(position){
-        const queryURL = "https://geo.fcc.gov/api/census/block/find?latitude=" + position.coords.latitude + "&longitude=" + position.coords.longitude + "&format=json";
-        $.ajax({url: queryURL, method: "GET"}).then(function(response){
-            console.log("FCC API");
-            console.log(response.County.name);
-            $("#searchbar").val(response.County.name);
-        });
-    }
-
-    function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(findCounty);
-        } else {
-            alert("Geolocation is not supported by this browser.");
+function getCountyData(name){
+    // There are 100 counties
+    for(let i=0; i<100; i++){
+        // Compares county names and returns the correct one
+        const tempCoName = data.features[i].attributes.CONAME.toLowerCase();
+        const tempCounty = data.features[i].attributes.County.toLowerCase();
+        if(name === tempCoName || name === tempCounty){
+            return data.features[i].attributes;
         }
     }
+    return null;
+}
 
-    //function showPosition(position) {
-    //    alert("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
-    //}
+function findCounty(position){
+    const queryURL = "https://geo.fcc.gov/api/census/block/find?latitude=" + position.coords.latitude + "&longitude=" + position.coords.longitude + "&format=json";
+    $.ajax({url: queryURL, method: "GET"}).then(function(response){
+        console.log("FCC API");
+        console.log(response.County.name);
+        $("#searchbar").val(response.County.name);
+    });
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(findCounty);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+// EVENT LISTENERS \\
+
+$(".search").on("click", function(){
+    const name = $("#searchbar").val();
+    runAjax(name,2);
 });
+
+$(".findme").on("click", getLocation);
+
+// MAIN \\
+function main(){
+    runAjax("Transylvania", 1); // <---- You can type the county name here for testing
+}
+main();
--- assets/translator.js
+++ assets/translator.js
@@ -1,37 +1,64 @@
 
 function translateToEnglish(){
+    // Button
     $(".translator").text("ESPAÑOL");
     // Index
-    $("#tc").text("Total Cases");
-    $("#td").text("Total Deaths");
-    $("#r10").text("Rate Per 10k");
-    $("#hos").text("Hospitalized");
+    $("#tc").html("TOTAL CASES: <span id='totalcases' class='num'></span>");
+    $("#td").html("TOTAL DEATHS: <span id='totaldeaths' class='num'></span>");
+    $("#r10").html("RATE PER 10K: <span id='rateper10k' class='num'></span>");
+    $("#hos").html("HOSPITALIZED: <span id='hospitalized' class='num'></span>");
     // County
     $("#cnty").text("County Information");
     // Links
     $("#linkT").text("Links");
+    $("#link1").text("Helpful Links");
+    $("#link2").text("Click the tabs below to interact with our helpful links.");
+    $("#link3").text("Looking for work due to Covid? Here we list our top pick for help in finding a job.");
+    $("#link4").html("<a href='http://www.laborfinders.com/ '>Labor Finders by Labor Finders International of Herdersonville</a> are a temporary staffing and employment agency.");
+    $("#link5").html("<a href='https://www.ncworks.gov/vosnet/Default.aspx'>Career Center by NCWorks</a> is a helpful site that conviently helps with job searching, creating a resume and career information.");
+    $("#link6").html("<a href='https://www.telamon.org/'>National Frameworker Jobs Program by Telamon Corporation - North Carolina</a> helps farmworkers and other low income adults who need help improving their English and in turn lead to better jobs.");
+    $("#link7").text("Has money been an issue for you through this pandemic? Here we list our top picks for financial assistance.");
+    $("#link8").html("<a href='https://elcentronc.org/health-and-well-being/covid19-crisis-response-fund-application/'>COVID19 Crisis Response Fund by El Centro Hispano</a> helps families reach their needs during this pandemic living in Wake, Orange or Durham County.");
+    $("#link9").html("<a href='https://www.coronacommunity.org/index.html#form'>Supply Delivery Assistance by Corona Community</a> connects you with volunteers(where available) to have food delivered to you if you are at high risk for Covid-19.");
+    $("#link10").html("<a href='https://www.ncdhhs.gov/assistance/low-income-services/ food-nutrition-services-food-stamps'>Food and Nutrition Services by North Carolina  Department of Helath and Human Services</a> is an entitlement program that assists low-income households in purchasing food they need.");
+    $("#link11").text("Resources We Used");
+    $("#link12").html("<a href='http://coronavirus-disasterresponse.hub.arcgis.com'>COVID-19 GIS Hub</a><br>Get maps, datasets, applications, and more for coronavirus disease 2019 (COVID-19).");
+    $("#link13").html("<a href='http://geo.fcc.gov/api/census'>FCC API</a><br>Drive public data across the public through your own application.");
     // Abous us
     $("#about1").text("About Us");
-    $("#about2").html("We are Americans<br>We are North Carolinians<br>We are web developers<br>but most importantly, we are concerned...");
+    $("#about2").html("We are Americans<br>We are North Carolinians<br>We are web developers<br>But most importantly, we are concerned...");
     $("#about3").html("There's no need to beat around the bush. Covid-19 has been one of the most challenging trials our nation has faced in our time. It infects indiscriminately, without mercy, and continues to expand it's reach and firm it's grip on our health as we try to balance social norms with social distancing.<br>Sadly, its impact is felt even harder on our latino community. Over 33% of confirmed cases in the U.S. were of Hispanic or Latino descent, yet information and resources to help this community is scarce. It's our mission to pull in information from trusted, reliable sources to show you the current totals, updated daily, along with helpful tips, links and resources to connect you to the people who can help.<br>Together, we can achieve anything!");
 }
 
 function translateToSpanish(){
+    // Button
     $(".translator").text("ENGLISH");
     // Index
-    $("#tc").text("Casos Totales");
-    $("#td").text("Muertes Totales");
-    $("#r10").text("Tasa Por 10k");
-    $("#hos").text("Hospitalizado");
+    $("#tc").html("CASOS TOTALES: <span id='totalcases' class='num'></span>");
+    $("#td").html("MUERTES TOTALES: <span id='totaldeaths' class='num'></span>");
+    $("#r10").html("TASA POR 10K: <span id='rateper10k' class='num'></span>");
+    $("#hos").html("HOSPITALIZADO: <span id='hospitalized' class='num'></span>");
     // County
     $("#cnty").text("Información del Condado");
     // Links
     $("#linkT").text("Enlaces");
+    $("#link1").text("Enlaces Útiles");
+    $("#link2").text("Haga clic en las pestañas a continuación para interactuar con nuestros útiles enlaces.");
+    $("#link3").text("¿Buscas trabajo debido a Covid? Aquí enumeramos nuestra mejor selección para obtener ayuda para encontrar un trabajo.");
+    $("#link4").html("<a href='http://www.laborfinders.com/ '>Labor Finders by Labor Finders International of Herdersonville</a> son una agencia temporal de empleo y empleo.");
+    $("#link5").html("<a href='https://www.ncworks.gov/vosnet/Default.aspx'>Career Center by NCWorks</a> es un sitio útil que ayuda de manera práctica con la búsqueda de empleo, la creación de un currículum y la información profesional.");
+    $("#link6").html("<a href='https://www.telamon.org/'>National Frameworker Jobs Program by Telamon Corporation - North Carolina</a> ayuda a los trabajadores agrícolas y otros adultos de bajos ingresos que necesitan ayuda para mejorar su inglés y, a su vez, lograr mejores empleos.");
+    $("#link7").text("El dinero ha sido un problema para usted a través de esta pandemia? Aquí la lista de nuestras mejores opciones para la ayuda financiera.");
+    $("#link8").html("<a href='https://elcentronc.org/health-and-well-being/covid19-crisis-response-fund-application/'>COVID19 Crisis Response Fund by El Centro Hispano</a> ayuda a las familias a satisfacer sus necesidades durante esta pandemia en el condado de Wake, Orange o Durham.");
+    $("#link9").html("<a href='https://www.coronacommunity.org/index.html#form'>Supply Delivery Assistance by Corona Community</a> lo conecta con voluntarios (donde estén disponibles) para que le entreguen alimentos si tiene un alto riesgo de Covid-19.");
+    $("#link10").html("<a href='https://www.ncdhhs.gov/assistance/low-income-services/ food-nutrition-services-food-stamps'>Food and Nutrition Services by North Carolina  Department of Helath and Human Services</a> es un programa de asistencia social que ayuda a los hogares de bajos ingresos a comprar los alimentos que necesitan.");
+    $("#link11").text("Recursos Que Utilizamos");
+    $("#link12").html("<a href='http://coronavirus-disasterresponse.hub.arcgis.com'>COVID-19 GIS Hub</a><br>Obtenga mapas, conjuntos de datos, aplicaciones y más para la enfermedad por coronavirus 2019 (COVID-19).");
+    $("#link13").html("<a href='http://geo.fcc.gov/api/census'>FCC API</a><br>Genere datos públicos en todo el público a través de su propia aplicación.");
     // Abous us
     $("#about1").text("Sobre Nosotros");
-
-    $("#about2").html("Nosotros somos Americanos<br>Nosotros somos Carolinianos del Norte<br>Somos desarrolladores web");
-    $("#about3").html("Pero lo mas importante, estamos preocupados. No hay necesidad de andar por las ramas. Covid-19 ha sido una de las pruebas mas desafiantes que nuestra nacion ha enfrentado en nuestro tiemo. Infecta indiscriminadamente sin piedad, y coninua expandiendo su alcance y firme su control sobre nuestra salud minetras tratamos de equilibrar las normas sociales con el distanciamiento social.<br>Lamentablemente, su impacto se siente aun mas duro en nuestra comunidad latina. Mas del 33% de los casos confirmados en Carolina del Norte eran de ascendencia hispana o latina, sin embargo, los recursos de información para ayudar a esta comunidad son escasos. Nuestra mision es obtener información de fuentes confiables para mostrarle los totales actualizados, junto con enlaces y recursos utiles para conectarse con personas que pueden ayudar.<br>¡Juntos podemos lograr cualquier cosa!");
+    $("#about2").html("Nosotros somos Americanos<br>Nosotros somos Carolinianos del Norte<br>Somos desarrolladores web<br>Pero lo mas importante, estamos preocupados...");
+    $("#about3").html("No hay necesidad de andar por las ramas. Covid-19 ha sido una de las pruebas mas desafiantes que nuestra nacion ha enfrentado en nuestro tiemo. Infecta indiscriminadamente sin piedad, y coninua expandiendo su alcance y firme su control sobre nuestra salud minetras tratamos de equilibrar las normas sociales con el distanciamiento social.<br>Lamentablemente, su impacto se siente aun mas duro en nuestra comunidad latina. Mas del 33% de los casos confirmados en Carolina del Norte eran de ascendencia hispana o latina, sin embargo, los recursos de información para ayudar a esta comunidad son escasos. Nuestra mision es obtener información de fuentes confiables para mostrarle los totales actualizados, junto con enlaces y recursos utiles para conectarse con personas que pueden ayudar.<br>¡Juntos podemos lograr cualquier cosa!");
 }
 
 $(".translator").on("click", function(){
@@ -41,4 +68,5 @@ $(".translator").on("click", function(){
     } else{
         translateToSpanish();
     }
-})
+    main();
+});
